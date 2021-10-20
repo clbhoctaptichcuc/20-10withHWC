923e84072f38aecf50fd0605ea4800dbf93580c5
clbhoctaptichcuc <92862561+clbhoctaptichcuc@users.noreply.github.com>
2021-10-20 20:14:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     titleWeb: "Mừng ngày Phụ nữ Việt Nam 20/10",
     introTitle: 'Thân gửi các bạn nữ của gia đình HWC',
     introDesc: 'Best regards for our dearest girls',
-    btnIntro: '^^HiHi^^',
+    btnIntro: 'Hãy ấn vào đây để nhận quà nhaa ^^Hehe^^',
     title: 'hãy có một ngày 20/10 thật vui vẻ và hạnh phúc bên người thân nhá 🥰',
     desc: 'omke khum? trả lời. ',
     btnYes: 'Nhất định <33',
